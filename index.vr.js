c866d582c755cca7f498de0a5a291eeea34f55e7
Colin Fendrick <colin.fendrick@gmail.com>
2017-09-21 16:22:22 -0400

    New text and image

--- index.vr.js
+++ index.vr.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   AppRegistry,
   asset,
+  Image,
   Pano,
   Text,
   View,
@@ -12,20 +13,55 @@ export default class NewVRProject extends React.Component {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          width: 2,
+          alignItems: 'stretch',  
+          transform: [{ translate: [-1, 1, -5] }],
+        }}>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red' }}>
+            <Text style={{ fontSize: 0.2, textAlign: 'center' }}>Turn</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'purple' }}>
+            <Text style={{ fontSize: 0.2, textAlign: 'center' }}>Around</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'grey' }}>
+            <Text style={{ fontSize: 0.2, textAlign: 'center' }}>Dummy</Text>
+          </View>
+        </View>
+          <Text style={{ 
+            backgroundColor: 'purple', 
+            transform: [
+              { translate: [4, 3, 0] },
+              { rotateY: -90 }
+            ],
+            fontSize: 0.8 
           }}>
-          hello
-        </Text>
+            Keep going...
+          </Text>
+          <Text style={{
+            backgroundColor: 'red',
+            transform: [
+              { translate: [-10, 3, 0] },
+              { rotateY: 90 }
+            ],
+            fontSize: 0.8
+          }}>
+            Not this way!
+          </Text>
+        
+        <Image
+          source={require('./static_assets/giphy.gif')}
+          style={{
+            transform: [
+              { translate: [-3, 5, 8] },
+              { rotateY: 180 }
+            ],
+            height: 5,
+            width: 5
+          }}
+        />
       </View>
     );
   }
